f5f853905df1ad16e494425ef772869bc275b073
Dai Yu <lfeyao@gmail.com>
2016-02-14 10:59:33 -0800

    edit

--- plugins/starter.py
+++ plugins/starter.py
@@ -10,17 +10,18 @@ typing_sleep = 0
 greetings = ['Hi friend!', 'Hello there.', 'Howdy!', 'Wazzzup!!!', 'Hi!', 'Hey.']
 help_text = "{}\n{}\n{}\n{}\n{}\n{}".format(
     "I will respond to the following messages: ",
-    "`pybot hello` for a random greeting.",
-    "`pybot joke` for a question, typing indicator, then answer style joke.",
-    "`pybot attachment` to see a Slack attachment message.",
+    "`baymax hello` for a random greeting.",
+    "`baymax joke` for a question, typing indicator, then answer style joke.",
+    "`baymax attachment` to see a Slack attachment message.",
+    "`baymax nba score` to see latest nba scores.",
     "`@<your bot's name>` to demonstrate detecting a mention.",
-    "`pybot help` to see this again.")
+    "`baymax help` to see this again.")
 
 # regular expression patterns for string matching
-p_bot_hi = re.compile("pybot[\s]*hi")
-p_bot_joke = re.compile("pybot[\s]*joke")
-p_bot_attach = re.compile("pybot[\s]*attachment")
-p_bot_help = re.compile("pybot[\s]*help")
+p_bot_hi = re.compile("baymax[\s]*hi")
+p_bot_joke = re.compile("baymax[\s]*joke")
+p_bot_attach = re.compile("baymax[\s]*attachment")
+p_bot_help = re.compile("baymax[\s]*help")
 
 def process_message(data):
     logging.debug("process_message:data: {}".format(data))
@@ -40,11 +41,11 @@ def process_message(data):
     elif p_bot_help.match(data['text']):
         outputs.append([data['channel'], "{}".format(help_text)])
 
-    elif data['text'].startswith("pybot"):
+    elif data['text'].startswith("baymax"):
         outputs.append([data['channel'], "I'm sorry, I don't know how to: `{}`".format(data['text'])])
 
     elif data['channel'].startswith("D"):  # direct message channel to the bot
-        outputs.append([data['channel'], "Hello, I'm the BeepBoop python starter bot.\n{}".format(help_text)])
+        outputs.append([data['channel'], "Hello, I'm bot created by Dan to help with your needs.\n{}".format(help_text)])
 
 def process_mention(data):
     logging.debug("process_mention:data: {}".format(data))
